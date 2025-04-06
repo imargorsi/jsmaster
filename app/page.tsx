a98a60a242a77fee3b8fb30d2a5cf9e618295b83
Muhammad Arsalan <m.arsalan@logicalcreations.net>
2025-04-06 15:04:47 +0500

    fixed tailwind issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-black text-primary">New App</h1>
+      <h1 className="text-3xl font-black text-primary-500">New App</h1>
     </div>
   );
 }
